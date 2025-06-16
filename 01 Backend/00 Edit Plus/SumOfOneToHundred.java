7148bd5cf758fb50e9e9837cd0894899e54c8ce3
Ronak Parmar <parmarronak21112003@gmail.com>
2025-06-16 20:53:59 +0530

    remove some files and change some files

---
+++ 01 Backend/00 Edit Plus/SumOfOneToHundred.java
@@ -0,0 +1,9 @@
+class SumOfOneToHundred {
+	public static void main(String[] arg) {
+		double sum = 0;
+		for(int i=1;i<=1000;i++) {
+			sum += i;
+		}
+		System.out.println("Sum is : " + sum);
+	}
+}
