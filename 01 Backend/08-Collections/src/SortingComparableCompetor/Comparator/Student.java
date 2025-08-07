5cb0f5e41dbcc12e03cf9556411f21800ebe984d
Ronak Parmar <parmarronak21112003@gmail.com>
2025-08-07 19:55:34 +0530

    comparator

---
+++ 01 Backend/08-Collections/src/SortingComparableCompetor/Comparator/Student.java
@@ -0,0 +1,56 @@
+package SortingComparableCompetor.Comparator;
+
+public class Student implements Comparable<Student> {
+    int roll;
+    String name;
+    int marks;
+
+    public Student(int roll, String name, int marks) {
+        this.roll = roll;
+        this.name = name;
+        this.marks = marks;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "roll=" + roll +
+                ", name='" + name + '\'' +
+                ", marks=" + marks +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        return this.name.compareTo(o.name);  //sorting based on name
+        //only one logic in comparable
+        //fixed logic in comparable
+        //one at time in comparable
+        //we can use multiple sorting logic in comparator
+
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+}
