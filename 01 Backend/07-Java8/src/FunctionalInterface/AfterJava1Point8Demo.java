2b9c1fabb7ea82c0dfa5915c43d6f6cbdd67fd3f
Ronak Parmar <parmarronak21112003@gmail.com>
2025-08-19 19:55:46 +0530

    add functional interface with multiple line

--- 01 Backend/07-Java8/src/FunctionalInterface/AfterJava1Point8Demo.java
+++ 01 Backend/07-Java8/src/FunctionalInterface/AfterJava1Point8Demo.java
@@ -9,6 +9,12 @@ public interface AfterJava1Point8Demo {
 class AfterJava1Point8 {
     public static void main(String[] args) {
         AfterJava1Point8Demo aj1p8 = (a, b) -> a + b;
+        AfterJava1Point8Demo afterJava1Point8Demo = (a, b) -> {
+            System.out.print("Lambda Expression : ");
+            return a + b;
+        };
         System.out.println(aj1p8.add(5, 10));
+
+        System.out.println(afterJava1Point8Demo.add(10, 30));
     }
 }
