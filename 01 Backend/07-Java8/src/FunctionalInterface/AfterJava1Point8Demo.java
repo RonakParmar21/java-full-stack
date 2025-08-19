b8e9b9a1ae4f2e00b31af4726bc56e12322202c2
Ronak Parmar <parmarronak21112003@gmail.com>
2025-08-19 19:57:05 +0530

    changes in functional interface

--- 01 Backend/07-Java8/src/FunctionalInterface/AfterJava1Point8Demo.java
+++ 01 Backend/07-Java8/src/FunctionalInterface/AfterJava1Point8Demo.java
@@ -8,13 +8,16 @@ public interface AfterJava1Point8Demo {
 
 class AfterJava1Point8 {
     public static void main(String[] args) {
+        // first way with single line in functional interface.
         AfterJava1Point8Demo aj1p8 = (a, b) -> a + b;
+        System.out.println(aj1p8.add(5, 10));
+
+
+        // second way with multiple line in functional interface.
         AfterJava1Point8Demo afterJava1Point8Demo = (a, b) -> {
             System.out.print("Lambda Expression : ");
             return a + b;
         };
-        System.out.println(aj1p8.add(5, 10));
-
         System.out.println(afterJava1Point8Demo.add(10, 30));
     }
 }
