e57895c0da0f41fa9f36616a413cf81d181886dc
Ronak Parmar <parmarronak21112003@gmail.com>
2025-08-31 11:21:52 +0530

    spring security notes

--- 01 Backend/07-Java8/src/LambdaDemo/ThreadImplementationWithLambdaExpression.java
+++ 01 Backend/07-Java8/src/LambdaDemo/ThreadImplementationWithLambdaExpression.java
@@ -3,19 +3,13 @@ package LambdaDemo;
 public class ThreadImplementationWithLambdaExpression {
     public static void main(String[] args) {
         Runnable task = () -> {
-            for(int i=0;i<50;i++)
-            {
+            for(int i=0;i<50;i++) {
                 System.out.println("hello...");
             }
         };
-
         new Thread(task).start();
-
-        for (int i=0;i<50;i++)
-        {
+        for (int i=0;i<50;i++) {
             System.out.println("main...");
         }
-
-
     }
 }
