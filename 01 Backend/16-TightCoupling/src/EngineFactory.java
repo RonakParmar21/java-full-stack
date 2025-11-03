1d866cacab6827846f592dab63e40063ec8b91b9
Ronak Parmar <parmarronak21112003@gmail.com>
2025-11-04 01:31:06 +0530

    some changes

--- 01 Backend/16-TightCoupling/src/EngineFactory.java
+++ 01 Backend/16-TightCoupling/src/EngineFactory.java
@@ -1,12 +1,9 @@
 public class EngineFactory {
-    public static Engine getEngine(String type)
-    {
-        if(type.equalsIgnoreCase("diesel"))
-        {
+    public static Engine getEngine(String type) {
+        if(type.equalsIgnoreCase("diesel")) {
             return new DieselEngine();
         }
-        else if(type.equalsIgnoreCase("petrol"))
-        {
+        else if(type.equalsIgnoreCase("petrol")) {
             return new PertolEngine();
         }
         else {
