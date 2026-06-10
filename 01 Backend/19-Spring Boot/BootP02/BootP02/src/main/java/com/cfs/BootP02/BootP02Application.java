73c9836115186b0c0913f4952a02c7896025f331
Ronak Parmar <parmarronak21112003@gmail.com>
2026-06-10 19:21:56 +0530

    Spring boot understanding

---
+++ 01 Backend/19-Spring Boot/BootP02/BootP02/src/main/java/com/cfs/BootP02/BootP02Application.java
@@ -0,0 +1,16 @@
+package com.cfs.BootP02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.webmvc.autoconfigure.WebMvcAutoConfiguration;
+
+// if I want to do not auto configure than use this
+//@SpringBootApplication(exclude = {WebMvcAutoConfiguration.class})
+@SpringBootApplication
+public class BootP02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootP02Application.class, args);
+	}
+
+}
